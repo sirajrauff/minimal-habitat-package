9106285cb1e65af44ec3ce26357f14ef78b95953
Siraj Rauff <sirajrauff@gmail.com>
2022-02-10 14:59:18 -0500

    Initial commit
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minimal Habitat Package
+
+Packge in it's most basic form, useful for testing CI/CD, libraries, etc. that require a package
---
+++ plan.sh
@@ -0,0 +1,12 @@
+pkg_name=noop
+pkg_origin=sirajrauff
+pkg_version="0.1.0"
+pkg_svc_run="sleep infinity"
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
